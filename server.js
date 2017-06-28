1c22c7687b8d9bb7b1dd7416e8f20000191f2043
Jess Poemape <jesspoemape@gmail.com>
2017-06-28 09:02:10 -0600

    handline requests with params 1-3; 4-6 pending

--- server.js
+++ server.js
@@ -32,9 +32,6 @@ app.get('/api/users', (req, res) => {
     } 
 });
 
-
-
-
 // get all users who drive a car newer than given year
 
 app.get('/api/users/vehicleYear', (req, res) => {
@@ -48,6 +45,7 @@ app.get('/api/users/vehicleYear', (req, res) => {
 })
 
 // update users email address to null with given last name
+
 app.put('/api/user/emailUpdate', (req, res) => {
     for (var i = 0; i < users.length; i++) {
         if (users[i].last_name === req.query.lastName) {
@@ -55,6 +53,74 @@ app.put('/api/user/emailUpdate', (req, res) => {
             res.send(users[i]);
         } 
     }
+});
+
+// update car make with given user id and car make 
+// ?userId=8&make=Ford
+
+app.put('/api/user/updateVehicle', (req, res) => {
+    for (var i = 0; i < users.length; i++) {
+        if (i - req.query.userId === -1) {
+            users[i].make = req.query.make;
+            res.send(users[i]);
+        }
+    }
+});
+
+app.delete('/api/removeUser', (req, res) => {
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].model === req.query.model) {
+            users.splice(i, 1);
+        }
+    }
+    res.send(`Users with ${req.query.model} model cars have been removed`);
+});
+
+ // Get user by id. The user id should be sent in the request url as a param
+ app.get('/api/user/:id', (req, res) => {
+    res.send(users[req.params.id - 1]);
+ });
+
+ app.get('/api/vehicles/:model', (req, res) => {
+     var usersArr = [];
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].model === req.params.model) {
+            usersArr.push(users[i]);
+        }
+    } 
+    res.send(usersArr);
+ });
+
+ // Get users whose first name starts with the given letter
+app.get('/api/userByName/:letter', (req, res) => {
+
+    var usersArr = [];
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].first_name.charAt(0) === req.params.letter) {
+            usersArr.push(users[i]);
+        }
+    }
+    res.send(usersArr);
+});
+
+// Update users name to 'Ned' by given id
+app.put('/api/updateName/:id', (req, res) => {
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].id === req.params.id) {
+            users[i].first_name = 'Ned';
+            res.send(users[i]);
+        }
+    }
+});
+
+// Delete user by id
+app.delete('/api/user/:id', (req, res) => {
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].id === req.params.id) {
+            users.splice(i, 1);
+        }
+    }
+    res.send(`User with ID of ${req.params.id} has been deleted`);
 });
 
 
