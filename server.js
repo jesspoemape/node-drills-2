713c411ecd5e4af93013c188fe84c885c28d888d
Jess Poemape <jesspoemape@gmail.com>
2017-06-27 22:15:22 -0600

    four end points made

--- server.js
+++ server.js
@@ -8,7 +8,54 @@ app.use(bodyParser.json());
 
 // === ENDPOINTS =========================
 
+app.get('/api/users', (req, res) => {
+    var usersArr = [];
 
+//get all users that drive an audi
+    if (req.query.make) {
+        for (var i = 0; i < users.length; i++) {
+            if (users[i].make === req.query.make) {
+            usersArr.push(users[i]);
+            }
+        }
+        res.send(usersArr);
+    }
+
+// get all users who's name starts with an A
+    else if (req.query.letter) {
+        for (var i = 0; i < users.length; i++) {
+        if (users[i].first_name.charAt(0) === req.query.letter) {
+            usersArr.push(users[i]);
+        }
+    }
+    res.send(usersArr);
+    } 
+});
+
+
+
+
+// get all users who drive a car newer than given year
+
+app.get('/api/users/vehicleYear', (req, res) => {
+    var usersArr = [];
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].year > req.query.year) {
+            usersArr.push(users[i]);
+        }
+    }
+    res.send(usersArr);
+})
+
+// update users email address to null with given last name
+app.put('/api/user/emailUpdate', (req, res) => {
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].last_name === req.query.lastName) {
+            users[i].email = null;
+            res.send(users[i]);
+        } 
+    }
+});
 
 
 
